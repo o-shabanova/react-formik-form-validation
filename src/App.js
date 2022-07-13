494e114f3826e1079a1456e86dacd944e245f1de
Olga Shabanova <shabanova994@gmail.com>
2022-07-13 21:31:56 +0300

    Add two components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import BasicForm from "./components/BasicForm";
+import AdvancedForm from "./components/AdvancedForm";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [view, setView] = useState("basic");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+        <nav>
+          <h3 onClick={() => setView("basic")} style={{color: view === "basic" ? "#fff" : ""}}>
+            Basic
+          </h3>
+          <h3 onClick={() => setView("advanced")}
+          style={{color: view === "advanced" ? "#f0f" : ""}}>
+            Advanced
+          </h3>
+        </nav>
+        {view === "basic" ? <BasicForm /> : <AdvancedForm />}
+      </div>
+  )
 }
-
 export default App;
+
+
+
